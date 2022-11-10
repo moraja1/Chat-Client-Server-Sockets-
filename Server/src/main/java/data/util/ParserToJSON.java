9cc3cc526ac6271903d49bfa945b7b6eab309ff8
moraja1 <jeisonmora0115@gmail.com>
2022-11-09 20:08:11 -0600

    Implemented JSON by Socket

--- Server/src/main/java/data/util/ParserToJSON.java
+++ Server/src/main/java/data/util/ParserToJSON.java
@@ -1,22 +1,17 @@
 package data.util;
 
-
 import data.model.repository.User;
-import data.util.jsonAdapters.UserAdapter;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
-import jakarta.json.bind.JsonbConfig;
 
 public class ParserToJSON {
-    private static final JsonbConfig config = new JsonbConfig().withAdapters(new UserAdapter());
-
     public static String UserToJson(User user){
-        Jsonb jsonb = JsonbBuilder.create(config);
+        Jsonb jsonb = JsonbBuilder.create();
         String userJSON = jsonb.toJson(user);
         return userJSON;
     }
     public static User JsonToUser(String jsonUser){
-        Jsonb jsonb = JsonbBuilder.create(config);
+        Jsonb jsonb = JsonbBuilder.create();
         User user = jsonb.fromJson(jsonUser, User.class);
         return user;
     }
